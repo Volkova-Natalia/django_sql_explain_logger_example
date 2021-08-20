85561e9b34451e42438d7dee40cf128024db6611
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-08-20 14:20:39 +0300

    (fix) explain - only for MySQL.

--- project_sample/backend_django/apps/sample/utils/sql_explain_logger.py
+++ project_sample/backend_django/apps/sample/utils/sql_explain_logger.py
@@ -1,17 +1,19 @@
 import logging
 from django.db import connection
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
 
 def sql_explain_logger():
-    for i, query in enumerate(connection.queries):
-        sql = query['sql']
-        if 'SELECT' in sql or 'DELETE' in sql or 'INSERT' in sql or 'REPLACE' in sql or 'UPDATE' in sql:
-            sql_explain = 'EXPLAIN FORMAT=JSON ' + sql
-            msg = 'Execution Plan:\n' + sql_explain + '\n'
-            with connection.cursor() as cursor:
-                cursor.execute(sql_explain)
-                row = cursor.fetchone()
-                msg += row[0]
-            logger.debug(msg)
+    if settings.DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
+        for i, query in enumerate(connection.queries):
+            sql = query['sql']
+            if 'SELECT' in sql or 'DELETE' in sql or 'INSERT' in sql or 'REPLACE' in sql or 'UPDATE' in sql:
+                sql_explain = 'EXPLAIN FORMAT=JSON ' + sql
+                msg = 'Execution Plan:\n' + sql_explain + '\n'
+                with connection.cursor() as cursor:
+                    cursor.execute(sql_explain)
+                    row = cursor.fetchone()
+                    msg += row[0]
+                logger.debug(msg)
